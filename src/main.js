29029c4ae271d58e3da62388b8bcc4aa115fe595
eltonlima01 <eltoncvlima@gmail.com>
2026-03-17 20:29:45 -0300

    Update app

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 
-import {Graph} from "../model/Graph.js"
+import {Graph} from "../models/Graph.js"
+import {Stick} from "../models/Stick.js"
 import {Camera} from "../utils/Camera.js";
 
 main ();
@@ -11,7 +12,7 @@ function main ()
     scene.background = new THREE.Color (0x888888);
 
     const canvas = document.querySelector ("#c");
-    const renderer = new THREE.WebGLRenderer ({canvas: canvas});
+    const renderer = new THREE.WebGLRenderer ({canvas: canvas, antialias: true});
 
 ////////////////////////////////////////////////////////////////
 
@@ -31,21 +32,20 @@ function main ()
     const graph = new Graph (canvas);
     scene.add (graph.get ());
 
-    const geometry = new THREE.BoxGeometry (0.0965, 0.3915, 6.0015);
-    const material = new THREE.MeshPhongMaterial ({color: 0xeeee00});
+    const stick = new Stick (canvas, camera);
+    scene.add (stick.get ());
 
-    const stick = new THREE.Mesh (geometry, material);
-    stick.rotation.x = 0.5;
-    stick.rotation.y = 0.5;
+    ////////////////////////////////
 
     const ambientLight = new THREE.AmbientLight (0x888888, 2.5);
 
     const light = new THREE.DirectionalLight (0xffffff, 5);
-    light.position.set (5, 5, 5);
+    light.position.set (0, 10, 0);
 
-    scene.add (stick);
     scene.add (light);
     scene.add (ambientLight);
+
+    ////////////////////////////////
 
     function animate ()
     {
